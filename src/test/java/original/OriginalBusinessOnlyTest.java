566d1478a8fff7bcd211fbdc4a531eecc76d033d
ma wei <wma@thoughtworks.com>
2015-03-19 12:44:12 +0800

    remove unused imports

--- src/test/java/original/OriginalBusinessOnlyTest.java
+++ src/test/java/original/OriginalBusinessOnlyTest.java
@@ -3,11 +3,8 @@ package original;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
--- src/test/java/proxy/WithProxyTest.java
+++ src/test/java/proxy/WithProxyTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
